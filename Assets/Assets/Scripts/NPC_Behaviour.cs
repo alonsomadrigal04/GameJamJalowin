76ece5d7f900a747018ccd9304169b83d1c7e145
Joan <jeruizko10@gmail.com>
2023-10-28 16:56:24 +0200

    NPC only runs towards a direction

--- Assets/Assets/Scripts/NPC_Behaviour.cs
+++ Assets/Assets/Scripts/NPC_Behaviour.cs
@@ -8,18 +8,23 @@ public class NPC_Behaviour : MonoBehaviour
     [SerializeField] float maxWalkTime = 3.0f;
     [SerializeField] float minIdleTime = 1.0f;
     [SerializeField] float maxIdleTime = 3.0f;
+    [SerializeField] GameObject player;
 
     Rigidbody2D rb;
+    Transform tfPlayer;
     Vector2 dir;
+    Vector2 runAwayDir;
     float currentSpeed;
     float maxSpeed;
     float moveTimer;
     bool isWalking = false;
+    bool isDragged = false;
     public bool scared = false;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        tfPlayer = player.GetComponent<Transform>();
         moveTimer = Random.Range(minIdleTime, maxIdleTime);
     }
 
@@ -30,37 +35,35 @@ public class NPC_Behaviour : MonoBehaviour
 
     void UpdateMovement()
     {
-        //Asignar valores a variables según la situación
-
-        maxSpeed = isWalking ? walkSpeed : 0;
-        currentSpeed = scared ? runSpeed : walkSpeed;
-
         //Arranque y detención
 
-        if (scared)
+        if (scared) rb.velocity = runAwayDir * currentSpeed;
+        else if (moveTimer <= 0)
         {
-            if (moveTimer < minWalkTime) moveTimer = Random.Range(minWalkTime, maxWalkTime);
-            if (!isWalking) isWalking = true;
-        }
-        else
-        {
-            if (moveTimer <= 0)
+            if (isWalking) //PASEANDO
             {
-                if (isWalking) //Le toca estar fokin parado
-                {
-                    isWalking = false;
-                    rb.velocity = Vector2.zero;
-                    moveTimer = Random.Range(minIdleTime, maxIdleTime);
-                }
-                else //Le toca estar fokin moviéndose
-                {
-                    isWalking = true;
-                    GetRandomDirection();
-                    moveTimer = Random.Range(minWalkTime, maxWalkTime);
-                }
+                isWalking = false;
+                moveTimer = Random.Range(minIdleTime, maxIdleTime);
+
+                rb.velocity = Vector2.zero;
             }
-            else moveTimer -= Time.deltaTime;
+            else //ESPERANDO
+            {
+                isWalking = true;
+                float randomAngle = Random.Range(0f, 360f);
+                dir = new Vector2(Mathf.Cos(randomAngle * Mathf.Deg2Rad), Mathf.Sin(randomAngle * Mathf.Deg2Rad));
+                dir.Normalize();
+                moveTimer = Random.Range(minWalkTime, maxWalkTime);
+
+                rb.velocity = dir * currentSpeed;
+            }
         }
+        else moveTimer -= Time.deltaTime;
+
+        //Asignar valores a variables según la situación
+
+        currentSpeed = scared ? runSpeed : walkSpeed;
+        maxSpeed = isDragged ? tfPlayer.moveSpeed : (isWalking ? currentSpeed : 0);
 
         //Limitar velocidad
 
@@ -70,16 +73,25 @@ public class NPC_Behaviour : MonoBehaviour
         rb.velocity = new Vector2(limitedXSpeed, limitedYSpeed);
     }
 
-    void GetRandomDirection()
+    void GetScared()
     {
-        float randomAngle = Random.Range(0f, 360f);
-        dir = new Vector2(Mathf.Cos(randomAngle * Mathf.Deg2Rad), Mathf.Sin(randomAngle * Mathf.Deg2Rad));
-        dir.Normalize();
-        rb.velocity = dir * walkSpeed;
+        scared = true;
+        moveTimer = Random.Range(minWalkTime, maxWalkTime);
+        isWalking = true;
+
+        Transform tfNPC = gameObject.GetComponent<Transform>();
+        Vector2 dir2Player = tfPlayer.position - tfNPC.position;
+        runAwayDir = -dir2Player.normalized;
     }
 
     public void Death()
     {
         Destroy(gameObject);
     }
+
+    //Si choca con el jugador, se puede arrastrar a la velocidad de este
+    void OnCollisionEnter(Collision other) { if (other.gameObject.CompareTag("Player")) isDragged = true; }
+    //Si no choca, no
+    void OnCollisionExit(Collision other) { if (other.gameObject.CompareTag("Player")) isDragged = false; }
+
 }
